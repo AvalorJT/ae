f741e99022d88ce1624a1401acd2b00914355f53
Avalor <avalor.j.t@gmail.com>
2025-06-12 23:07:44 +0300

    Added basic example api

--- AutomationEngine.py
+++ AutomationEngine.py
@@ -1,11 +1,7 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from PySide6.QtGui import QGuiApplication # NEW: Import QGuiApplication
-from PySide6.QtCore import Qt # NEW: Import Qt for the policy enum
-
 from GUI.MainWindow import MainWindow
 
-QGuiApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.Round)
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
